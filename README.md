c3af637baa53f7e05cdf3e6be2d80b50ec115f39
dndjjhsfjhj <92665212+dndjjhsfjhj@users.noreply.github.com>
2023-02-23 23:57:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# practice-session
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-xcaf33)
---
+++ script.js
@@ -0,0 +1,13 @@
+//Hoisting in js
+a=2
+console.log(a)
+var a
+
+Hoist()
+function Hoist(){
+  console.log("Functional Hoisting")
+}
+//coersion in javascript
+console.log(1+"1")
+console.log(1-"1")
+console.log("komal"+"Deshmukh")
